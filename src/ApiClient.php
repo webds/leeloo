71a3019bea5ca882c6ef979d8e74d2289f7effcc
Romeo <info@webds.net>
2019-04-28 14:16:58 +0300

    Add files via upload

---
+++ src/ApiClient.php
@@ -0,0 +1,182 @@
+<?php
+
+/*
+ * Leeloo REST API Client
+ * Roman WebDS  info@webds.net  Telegram WebDS_Net
+ * Documentation
+ * https://leelooai.atlassian.net/wiki/spaces/DOC/overview
+ *
+ */
+
+namespace Leeloo;
+use stdClass;
+
+class ApiClient implements ApiInterface
+{
+
+    private $apiUrl = 'https://api.leeloo.ai/api/v1/';
+
+    private $TokenRead;
+    private $TokenWrite;
+
+    /**
+     * Sendpulse API constructor
+     *
+     * @param                       $userId
+     * @param                       $secret
+     * @param TokenStorageInterface $tokenStorage
+     *
+     * @throws Exception
+     */
+    public function __construct($TokenWrite,$TokenRead = "")
+    {
+
+        if (empty($TokenWrite)) {
+          return false;//('Empty ID or TokenWrite');
+        }
+
+        $this->TokenRead = $TokenRead;
+        $this->TokenWrite = $TokenWrite;
+    }
+
+
+
+    /**
+     * Form and send request to API service
+     *
+     * @param        $path
+     * @param string $method
+     * @param array $data
+     * @param bool $useToken
+     *
+     * @return stdClass
+     */
+    protected function sendRequest($path, $method = 'GET', $data = array(), $useToken = true)
+    {
+        $url = $this->apiUrl . '/' . $path;
+        $method = strtoupper($method);
+        $curl = curl_init();
+
+        if ($useToken && !empty($this->token)) {
+            $headers = array('X-Leeloo-AuthToken: ' . $this->TokenWrite);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
+
+        switch ($method) {
+            case 'POST':
+                curl_setopt($curl, CURLOPT_POST, count($data));
+                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+                break;
+            case 'PUT':
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+                break;
+            case 'DELETE':
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+                break;
+            default:
+                if (!empty($data)) {
+                    $url .= '?' . http_build_query($data);
+                }
+        }
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 15);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 15);
+
+        $response = curl_exec($curl);
+        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        $headerCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $responseBody = substr($response, $header_size);
+
+        curl_close($curl);
+
+        if ($headerCode === 401 && $this->refreshToken === 0) {
+            ++$this->refreshToken;
+            $this->getToken();
+            $retval = $this->sendRequest($path, $method, $data);
+        } else {
+            $retval = new stdClass();
+            $retval->data = json_decode($responseBody);
+            $retval->http_code = $headerCode;
+        }
+
+        return $retval;
+    }
+
+    /**
+     * Process results
+     *
+     * @param $data
+     *
+     * @return stdClass
+     */
+    protected function handleResult($data)
+    {
+        if (empty($data->data)) {
+            $data->data = new stdClass();
+        }
+        if ($data->http_code !== 200) {
+            $data->data->is_error = true;
+            $data->data->http_code = $data->http_code;
+        }
+
+        return $data->data;
+    }
+
+    /**
+     * Process errors
+     *
+     * @param null $customMessage
+     *
+     * @return stdClass
+     */
+    protected function handleError($customMessage = null)
+    {
+        $message = new stdClass();
+        $message->is_error = true;
+        if (null !== $customMessage) {
+            $message->message = $customMessage;
+        }
+
+        return $message;
+    }
+
+
+    /*
+     * API interface implementation
+     */
+
+
+    /**
+     * Create address book
+     *
+     * @param $bookName
+     *
+     * @return stdClass
+     */
+    public function order($paymentCreditsId,
+                          $email,
+                          $phone,
+                          $transactionDate,
+                          $offerId,
+                          $accountId,
+                          $isNotifyAccount)
+    {
+        if (empty($bookName)) {
+            return $this->handleError('Empty book name');
+        }
+
+        $data = array('bookName' => $bookName);
+        $requestResult = $this->sendRequest('addressbooks', 'POST', $data);
+
+        return $this->handleResult($requestResult);
+    }
+
+
+}
---
+++ src/ApiInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * LeeLoo REST API Interface
+ * Roman WebDS Telegram WebDS_Net
+ * Documentation
+ * https://leelooai.atlassian.net/wiki/spaces/DOC/pages/465108998/Write+API#WriteAPI-%D0%94%D0%BE%D0%B1%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0
+ *
+ */
+
+namespace Leeloo;
+
+interface ApiInterface
+{
+
+    /**
+     * Create new address book
+     *
+     * @param $bookName
+     */
+    public function order(
+        $paymentCreditsId,
+        $email,
+        $phone,
+        $transactionDate,
+        $offerId,
+        $accountId,
+        $isNotifyAccount
+    );
+
+
+}
